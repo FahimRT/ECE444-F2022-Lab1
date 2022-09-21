db899920b66edf5707dbf9dbaa5f8d527adc8558
Shaibal Muhtadee <shaibal.muhtadee@mail.utoronto.ca>
2022-09-21 18:09:38 -0400

    made the change

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,1 @@
-<<<<<<< HEAD
-print(f"Hello WorldFahim")
-# Merge Resolved
-
-![Merge Resolved](Image 2022-09-21 at 5.41 PM.jpg)
-=======
-print(f"Hello World5")
->>>>>>> b7ed0e87f274465be645fbe6839191723288f9b8
+print(f"Hello World5 sup")
